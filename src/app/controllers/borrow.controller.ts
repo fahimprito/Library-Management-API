6efb328cf781bcba697cba5b9493c612549f37b5
fahimprito <ahmmedahmmed75@gmail.com>
2025-06-23 02:01:45 +0600

    borrowBook summary

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -48,6 +48,53 @@ const borrowBook = async (req: Request, res: Response): Promise<any> => {
     }
 };
 
+const getBorrowSummary = async (req: Request, res: Response) => {
+    try {
+        const summary = await Borrow.aggregate([
+            {
+                $group: {
+                    _id: '$book',
+                    totalQuantity: { $sum: '$quantity' },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'books',
+                    localField: '_id',
+                    foreignField: '_id',
+                    as: 'bookInfo',
+                },
+            },
+            {
+                $unwind: '$bookInfo',
+            },
+            {
+                $project: {
+                    _id: 0,
+                    book: {
+                        title: '$bookInfo.title',
+                        isbn: '$bookInfo.isbn',
+                    },
+                    totalQuantity: 1,
+                },
+            },
+        ]);
+
+        res.status(200).json({
+            success: true,
+            message: 'Borrowed books summary retrieved successfully',
+            data: summary,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong',
+            error,
+        });
+    }
+};
+
 export const borrowController = {
-    borrowBook
+    borrowBook,
+    getBorrowSummary
 };
